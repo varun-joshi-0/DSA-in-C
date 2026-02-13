acbb0d08819a1dd78c9463f4749d0190b93acfd9
varun-joshi-0 <varunjoshi421@gmail.com>
2026-02-13 15:20:18 +0530

    Implement matrix addition and sum calculations
    This program performs addition of two matrices by summing corresponding elements and calculates the sum of each row and column.

--- 100-Days-Of-Code/Day11.c
+++ 100-Days-Of-Code/Day11.c
@@ -1,1 +1,52 @@
+//Write a program to perform addition of two matrices having the same dimensions. 
+//The sum of two matrices is obtained by adding corresponding elements of the matrices.
 
+#include<stdio.h>
+
+int main(){
+
+    int r , c ;
+    printf("Enter the Number of Rows: ");
+    scanf("%d",&r);
+    printf("Enter the Number of Columns: ");
+    scanf("%d",&c);
+
+    int mat[r][c];
+
+    printf("Enter the Elements: ");
+    for(int i = 0 ; i < r ; i++){
+        for(int j = 0 ; j < c ; j++){
+            scanf("%d",&mat[i][j]);
+        }
+    }
+
+    for(int i = 0 ; i < r ; i++){
+        for(int j = 0 ; j < c ; j++){
+            printf("%d  ",mat[i][j]);
+        }
+        printf("\n");
+    }
+
+    int sumr, sumrt = 0 , sumc , sumct = 0;
+    for(int i = 0 ; i < r ; i++){
+        sumr = 0 ;
+        for(int j = 0 ; j < c ; j ++){
+            sumr = sumr + mat[i][j];
+            sumrt = sumrt + mat[i][j];
+        }
+        printf("Sum of Row%d: %d\n", i+1 , sumr);
+    }
+    printf("Sum of all Rows: %d\n" , sumrt);
+
+    for(int i = 0 ; i < c ; i++){
+        sumc = 0 ;
+        for(int j = 0 ; j < r ; j ++){
+            sumc = sumc + mat[j][i];
+            sumct = sumct + mat[j][i];
+        }
+        printf("Sum of Column%d: %d\n", i+1 , sumc);
+    }
+    printf("Sum of all Columns: %d" , sumct);
+
+    return 0 ;
+}
