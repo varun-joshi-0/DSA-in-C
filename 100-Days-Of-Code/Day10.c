3094467717a302f1533060a3bafb3974a7f3562c
varun-joshi-0 <varunjoshi421@gmail.com>
2026-02-13 15:11:41 +0530

    Add palindrome check functionality in Day10.c
    Implement palindrome check using two-pointer method.

--- 100-Days-Of-Code/Day10.c
+++ 100-Days-Of-Code/Day10.c
@@ -1,1 +1,29 @@
+//Read a string and check if it is a palindrome using two-pointer comparison.
 
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100] , temp ;
+    fgets(str , 99 , stdin );
+    char str2[100] ;
+
+    
+    str[strcspn(str,"\n")] = '\0';
+    int b = strlen(str) ;
+    strcpy(str2,str);
+
+    for(int i = 0 ; i < b/2  ; i++){
+        temp  = str[i] ;
+        str[i] = str [b - i - 1];
+        str[b - i - 1] = temp ;
+    }
+
+    if ( strcmp(str,str2) == 0 ){
+        printf("Its a Palindrome");
+    }
+    else{
+        printf("Not a Palindrome");
+    }
+    
+    return 0 ;
+}
